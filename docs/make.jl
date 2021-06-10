201885228107f5c780db70599d28e6628abf3a18
kirstvh <73718004+kirstvh@users.noreply.github.com>
2021-06-10 15:38:21 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,10 +5,11 @@ using BioCCP
 makedocs(sitename="BioCCP",
     format = Documenter.HTML(),
     modules=[BioCCP],  
-    pages=Any["BioCCP"=> "man/BioCCP.md"])
+    pages=Any["BioCCP"=> "man/BioCCP.md",])
 
 #=
 deploydocs(
             repo = "github.com/kirstvh/BioCCP.jl.git",
         )
+
 =#
