8abb7da564643bf6f50214e029e30c97c0a0df0e
Kirsten Van Huffel <73718004+kirstvh@users.noreply.github.com>
2021-10-01 21:42:26 +0200

    Update BioCCP_Case_Study.jl

--- notebooks/BioCCP_Case_Study.jl
+++ notebooks/BioCCP_Case_Study.jl
@@ -6,10 +6,10 @@ using InteractiveUtils
 
 
 # ╔═╡ 666850df-859e-489c-b92b-c63b6260c190
- begin 
- 	using Pkg; Pkg.add(url="https://github.com/kirstvh/BioCCP.jl")
- 	Pkg.add(["Plots", "PlutoUI", "Distributions"])
-end
+#  begin 
+#  	using Pkg; Pkg.add(url="https://github.com/kirstvh/BioCCP.jl")
+#  	Pkg.add(["Plots", "PlutoUI", "Distributions"])
+# end
 
 # ╔═╡ e1a7f2da-a38b-4b3c-a238-076769e46408
 using Plots, PlutoUI, Distributions, BioCCP
